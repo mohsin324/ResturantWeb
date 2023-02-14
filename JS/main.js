21b91bb308f4c2d98fd7960d0ca5294528b054e7
Mohsin Munir <mohsinmunir324@gmail.com>
2023-02-14 23:03:06 +0500

    first commit

---
+++ JS/main.js
@@ -0,0 +1,24 @@
+// Get DOM element
+const navIconEl = document.querySelector('.nav__icon');
+const navCloseEl = document.querySelector('.nav__close');
+const navList = document.querySelector('.nav__list');
+const navBgOverlayEl = document.querySelector('.nav__bgOverlay');
+
+// create functions   navOpen()
+const navOpen = () => {
+    navList.classList.add("show");
+    navBgOverlayEl.classList.add("active");
+    document.body.style = 'visibility: visible; height: 100vh; width: 100vw; overflow-x: hidden;';
+
+}
+const navClose = () => {
+    navList.classList.remove("show");
+    navBgOverlayEl.classList.remove("active");
+    document.body.style = 'visibility: visible; height: 100vh; width: 100vw; overflow-x: hidden;';
+
+}
+
+navIconEl.addEventListener('click', navOpen);
+navCloseEl.addEventListener('click', navClose);
+navBgOverlayEl.addEventListener('click', navClose);
+
