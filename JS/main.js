f8a30029b04a0d0759ae4911ef3da811a607d1cb
Mohsin Munir <mohsinmunir324@gmail.com>
2023-02-15 17:07:11 +0500

    done with home page

--- JS/main.js
+++ JS/main.js
@@ -22,3 +22,14 @@ navIconEl.addEventListener('click', navOpen);
 navCloseEl.addEventListener('click', navClose);
 navBgOverlayEl.addEventListener('click', navClose);
 
+
+// aos init
+AOS.init({
+    offset: 200,
+    delay: 100,
+    duration: 400,
+    easing: 'ease',
+    once: false, 
+    mirror: false,
+    anchorPlacement: 'top-bottom'
+});
